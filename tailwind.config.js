4da8ddcef67c0ae3fb468a8cc76e788c74526a25
fasfashann <saintgloria.jkt@gmail.com>
2023-12-21 23:57:52 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        twilight: "#0F2021",
+        dusk: "#0F3538",
+        cloud: "#496767",
+        fog: "#87a19e",
+        radiance: "#ff6600",
+        warmth: "#ff9200",
+        shine: "#ffbe00",
+        gleam: "#f8e08e",
+        bright: "#fafaf0",
       },
     },
   },
   plugins: [],
-}
+};
