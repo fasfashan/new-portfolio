0b52435648602cc6162a6d6e96a5f46c9b2c0c4b
fasfashann <saintgloria.jkt@gmail.com>
2023-12-24 11:16:30 +0700

    fixing meta tags for twitter

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,18 @@ import Navbar from "./navbar";
 export const metadata = {
   title: "Fasha Fadillah Portfolio's",
   description: "My portfolio's",
+  twitter: {
+    card: "summary_large_image",
+    title: "Fasha Fadillah Portfolio's",
+    description: "This is my personal website which contains some of my work",
+    images: [
+      {
+        url: "/og_image.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
   openGraph: {
     title: "Fasha Fadillah Portfolio's",
     description: "This is my personal website which contains some of my work",
