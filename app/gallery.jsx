0256dc4bdaf401c52ebfec7f41c12dfc2f0db61d
fasfashann <saintgloria.jkt@gmail.com>
2023-12-27 16:29:36 +0700

    change layout gallery

--- app/gallery.jsx
+++ app/gallery.jsx
@@ -3,26 +3,26 @@ import Image from "next/image";
 export default function Gallery() {
   return (
     <>
-      <div className="grid md:grid-cols-4 xl:grid-cols-3 gap-5 mt-20 ">
-        <div className=" p-8 bg-bright ">
+      <div className="grid md:grid-cols-12 xl:grid-cols-12 gap-5 mt-20 ">
+        <div className="md:col-span-8 p-8 bg-bright ">
           <Image
-            className="m-auto hover:scale-95 transition-all"
+            className="m-auto shadow-xl rounded-md hover:scale-95 transition-all"
             alt="Saham Apps"
-            width="340"
-            height="340"
-            src="/saham.png"
-          />
-        </div>
-        <div className=" p-8 bg-gleam  ">
-          <Image
-            alt="Staycation Web App"
-            className="shadow-xl hover:scale-95 transition-all m-auto rounded-lg"
             width="340"
             height="340"
             src="/StaycationWeb.jpg"
           />
         </div>
-        <div className=" p-8 bg-bright  flex items-center  ">
+        <div className="md:col-span-4 flex p-8 bg-gleam  ">
+          <Image
+            alt="Staycation Web App"
+            className=" hover:scale-95 transition-all m-auto rounded-lg"
+            width="340"
+            height="340"
+            src="/saham.png"
+          />
+        </div>
+        <div className="md:col-span-4 p-8 bg-bright  flex items-center  ">
           <Image
             alt="ProWork"
             className="shadow-xl hover:scale-95 transition-all rounded-lg m-auto"
@@ -31,7 +31,7 @@ export default function Gallery() {
             src="/ProWork.jpg"
           />
         </div>
-        <div className=" p-8  bg-gleam flex items-center ">
+        <div className="md:col-span-8 p-8  bg-gleam flex items-center ">
           <Image
             className="rounded-lg hover:scale-95 transition-all m-auto shadow-xl"
             alt="Dashboard"
@@ -40,22 +40,22 @@ export default function Gallery() {
             src="/Dashboard.jpg"
           />
         </div>
-        <div className=" p-8 bg-bright flex ">
+        <div className="md:col-span-8 p-8 bg-bright flex ">
           <Image
             className="rounded hover:scale-95 transition-all m-auto shadow-xl"
             alt="BCA"
             width="340"
             height="340"
-            src="/BCA.jpg"
+            src="/Fashion Login.jpg"
           />
         </div>
-        <div className=" p-8 bg-gleam flex ">
+        <div className="md:col-span-4 p-8 bg-gleam flex ">
           <Image
             className="rounded hover:scale-95 transition-all m-auto shadow-xl"
             alt="Fashion Login"
             width="340"
             height="340"
-            src="/Fashion Login.jpg"
+            src="/BCA.jpg"
           />
         </div>
       </div>
--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import Hero from "./hero";
 import CaseStudy from "./case-study";
 import Footer from "./footer";
 export default function Home() {
-  const currentYear = new Date().getFullYear();
   return (
     <>
       <Hero />
