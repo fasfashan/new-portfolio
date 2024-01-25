e8d8266652de5f4b8d6f4765aa9729460626288e
fasfashann <saintgloria.jkt@gmail.com>
2024-01-25 15:37:47 +0700

    change url image and change og_image

--- app/projects/luxury-hotel-web-design/page.jsx
+++ app/projects/luxury-hotel-web-design/page.jsx
@@ -1,15 +1,7 @@
-import { metadata } from "@/app/layout";
-import Head from "next/head";
-
 import Projects from "@/app/template/projects.jsx";
 export default function Page() {
   return (
     <>
-      <Head>
-        <title>Luxury hotel landing page</title>
-        <meta name="description" content={metadata.description} />
-        {/* Add other meta tags as needed */}
-      </Head>
       <Projects
         project_name={"Luxury hotel landing page."}
         project_desc="Actually, this is a case study provided by one of the companies I applied to, which involves creating a landing page design for a luxury hotel."
--- app/projects/time-tree-redesign/page.jsx
+++ app/projects/time-tree-redesign/page.jsx
@@ -1,15 +1,7 @@
-import { metadata } from "@/app/layout";
-import Head from "next/head";
-
 import Projects from "@/app/template/projects.jsx";
 export default function Page() {
   return (
     <>
-      <Head>
-        <title>Luxury hotel landing page</title>
-        <meta name="description" content={metadata.description} />
-        {/* Add other meta tags as needed */}
-      </Head>
       <Projects
         project_name={"TimeTree Redesign Mobile App"}
         project_desc="Improve KPI for this App. Funnel from First App Downloads to Share Calendar with other friends and Input first calendar event."
