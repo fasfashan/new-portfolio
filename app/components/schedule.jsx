144f3ca130be01d0d911770bae14d04a63ae891b
fasfashann <saintgloria.jkt@gmail.com>
2024-03-05 10:00:40 +0700

    add round two

--- app/components/schedule.jsx
+++ app/components/schedule.jsx
@@ -1,9 +1,11 @@
 import RoundOne from "@/app/components/round-1";
+import RoundTwo from "@/app/components/round-2";
 export default function Schedule() {
   return (
     <>
       <div className="grid md:grid-cols-3 gap-8 grid-cols-1">
         <RoundOne />
+        <RoundTwo />
       </div>
     </>
   );
